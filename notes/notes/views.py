44aafcfb0a346adeda6a741cd6fbc9dd95d98779
Serzh-byte <serzhtadevosyan1@gmail.com>
2025-01-14 23:09:47 +0000

    Updates so far

--- notes/notes/views.py
+++ notes/notes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
+    topics = ['Machine Learning', 'Statistics', 'Data Science', 'AI']
     if request.method == 'POST':
-        return render(request, 'notes/home.html')
-    return render(request, 'notes/home.html')
+        return render(request, 'notes/home.html', {'topics':topics})
+    return render(request, 'notes/home.html', {'topics':topics})
